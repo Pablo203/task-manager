b4afd0954e2d841ae435f238f14ecb3dd8653464
Pablo203 <76450441+Pablo203@users.noreply.github.com>
2022-11-01 12:56:31 +0100

    Add new styles and navbar to login and registration

--- usersAuth/forms.py
+++ usersAuth/forms.py
@@ -12,7 +12,7 @@ class RegistrationUserForm(UserCreationForm):
     def __init__(self, *args, **kwargs):
         super(RegistrationUserForm, self).__init__(*args, **kwargs)
 
-        self.fields['username'].widget.attrs['class'] = ['form-control']
+        self.fields['username'].widget.attrs['class'] = 'form-control'
 
-        self.fields['password1'].widget.attrs['class'] = ['form-control']
-        self.fields['password2'].widget.attrs['class'] = ['form-control']
+        self.fields['password1'].widget.attrs['class'] = 'form-control'
+        self.fields['password2'].widget.attrs['class'] = 'form-control'
